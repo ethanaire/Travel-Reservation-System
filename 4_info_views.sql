aa2e037b53bc2777f34d7b17bc4e0e478dac0c72
Hai Hoang nguyen <hoangngn.26@gmail.com>
2025-04-12 23:35:18 +0700

    fix table names error

--- 4_info_views.sql
+++ 4_info_views.sql
@@ -1,7 +1,7 @@
 -- Select schema
 USE travel_reservation;
 
--- Customer Demographics View
+-- 1. Customer Demographics View
 -- Insight into the profile of customers (gender, age group, country).
 CREATE VIEW v_customer_demographics AS
 SELECT
@@ -9,31 +9,48 @@ SELECT
   country,
   gender,
   DATE_PART('year', AGE(current_date, date_of_birth)) AS age
-FROM Customers;
+FROM customers;
 
--- Revenue View
+-- 2. Revenue View
 -- Total spending by top individual customers
-CREATE VIEW customer_spending AS
+CREATE VIEW v_customer_spending AS
 SELECT
     c.customer_id,
     c.customer_first_name,
     c.customer_family_name,
     SUM(COALESCE(fb.price, 0) + COALESCE(hb.price, 0)) AS total_spent
-FROM Customers c
-LEFT JOIN FlightBookings fb ON c.customer_id = fb.customer_id
-LEFT JOIN HotelBookings hb ON c.customer_id = hb.customer_id
+FROM customers c
+LEFT JOIN flightbookings fb ON c.customer_id = fb.customer_id
+LEFT JOIN hotelbookings hb ON c.customer_id = hb.customer_id
 GROUP BY c.customer_id, c.customer_first_name, c.customer_family_name;
 
--- Flight Summary View 
+-- 3. Flight Summary View 
 -- Aggregated flight bookings by airline, route, and class
 CREATE VIEW v_flight_summary AS
 SELECT
-  fb.flight_id,
-  a.name AS airline,
+  fb.trip_id,
+  a.airline_name AS airline,
   fb.travel_class,
-  COUNT(*) AS total_bookings,
+  COUNT(*) AS total_flight_bookings,
   SUM(fb.price) AS total_revenue
+FROM flightbookings fb
+JOIN airlines a ON fb.airline_id = a.airline_id
+GROUP BY fb.flight_id, a.airline_name, fb.travel_class;
+
+-- 4. Top Routes View 
+-- Frequent and high-revenue flight routes.
+CREATE VIEW v_top_routes AS
+SELECT
+  fb.airport_src,
+  fb.airport_dst,
+  ap1.name AS departure_airport,
+  ap2.name AS arrival_airport,
+  COUNT(fb.booking_id) AS booking_count,
+  SUM(fb.price) AS revenue
 FROM flight_bookings fb
-JOIN flights f ON fb.flight_id = f.flight_id
-JOIN airlines a ON f.airline_id = a.airline_id
-GROUP BY fb.flight_id, a.name, fb.travel_class;
+JOIN airports ap1 ON fb.airport_src = ap1.airport_id
+JOIN airports ap2 ON fb.airport_dst = ap2.airport_id
+GROUP BY fb.airport_src, fb.airport_dst, ap1.name, ap2.name;
+
+-- 5. Hotel Booking Trends View
+-- Useful to analyze hotel check-in/check-out trends and revenue.
