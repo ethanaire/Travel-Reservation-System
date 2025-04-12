f1564b64acd858a33d2d0a02d07903bfe68d814d
Hai Hoang nguyen <hoangngn.26@gmail.com>
2025-04-12 21:51:28 +0700

    add customer demographics and flight summary views

--- 4_info_views.sql
+++ 4_info_views.sql
@@ -1,7 +1,18 @@
 -- Select schema
 USE travel_reservation;
 
+-- Customer Demographics View
+-- Insight into the profile of customers (gender, age group, country).
+CREATE VIEW v_customer_demographics AS
+SELECT
+  customer_id,
+  country,
+  gender,
+  DATE_PART('year', AGE(current_date, date_of_birth)) AS age
+FROM Customers;
+
 -- Revenue View
+-- Total spending by top individual customers
 CREATE VIEW customer_spending AS
 SELECT
     c.customer_id,
@@ -12,3 +23,17 @@ FROM Customers c
 LEFT JOIN FlightBookings fb ON c.customer_id = fb.customer_id
 LEFT JOIN HotelBookings hb ON c.customer_id = hb.customer_id
 GROUP BY c.customer_id, c.customer_first_name, c.customer_family_name;
+
+-- Flight Summary View 
+-- Aggregated flight bookings by airline, route, and class
+CREATE VIEW v_flight_summary AS
+SELECT
+  fb.flight_id,
+  a.name AS airline,
+  fb.travel_class,
+  COUNT(*) AS total_bookings,
+  SUM(fb.price) AS total_revenue
+FROM flight_bookings fb
+JOIN flights f ON fb.flight_id = f.flight_id
+JOIN airlines a ON f.airline_id = a.airline_id
+GROUP BY fb.flight_id, a.name, fb.travel_class;
